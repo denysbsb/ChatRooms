1b098e24b6eab81b0cfbbbc64915629bddcd4dc2
denysbsb <denys.design@gmail.com>
2017-01-24 17:41:11 -0200

    services path

---
+++ src/app/services/chat-messages.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFire } from 'angularfire2';
+import { ChatMessage } from '../models';
+
+@Injectable()
+export class ChatMessagesService {
+
+  constructor(private _af: AngularFire) { }
+
+
+ 
+
+}
