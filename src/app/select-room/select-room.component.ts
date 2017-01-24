816ebe17d668654522b39ce571dc4c5d999e4850
denysbsb <denys.design@gmail.com>
2017-01-24 17:34:53 -0200

    select room component

---
+++ src/app/select-room/select-room.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
+@Component({
+  selector: 'fc-select-room',
+  templateUrl: 'select-room.component.html',
+  styleUrls: ['select-room.component.css']
+})
+export class SelectRoomComponent implements OnInit {
+  rooms: FirebaseListObservable<any>;
+
+  constructor(private _af: AngularFire) {
+   
+  }
+
+  ngOnInit() {
+    this.rooms = this._af.database.list('/rooms/');
+    console.log(this.rooms);
+  }
+
+}
