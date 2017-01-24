2808e43acc894662eefbf34751043d2d4245939b
denysbsb <denys.design@gmail.com>
2017-01-24 17:33:11 -0200

    component chat/ init

---
+++ src/app/chat/chat.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'fc-chat',
+  templateUrl: 'chat.component.html',
+  styleUrls: ['chat.component.css']
+})
+export class ChatComponent {
+  roomId: string;
+
+  constructor(private _route:ActivatedRoute){
+    this.roomId = this._route.snapshot.params['roomId'];
+    console.log('MEU ID',this.roomId);
+  }
+}
